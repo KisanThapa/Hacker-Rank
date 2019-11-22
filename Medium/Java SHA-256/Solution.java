878afde1eeb209ac962ae3058794d99feb81bf93
Kisan Thapa <kisanthapa33@gmail.com>
2019-11-22 14:22:07 +0545

    SHA-256 hashing file

---
+++ Medium/Java SHA-256/Solution.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.security.MessageDigest;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        scanner.close();
+
+        String hashType = "SHA-256";
+
+        try{ 
+
+            MessageDigest mDigest = MessageDigest.getInstance(hashType);
+            mDigest.update(text.getBytes());
+            byte[] hash = mDigest.digest();
+
+           for (byte bt : hash) {
+                System.out.printf("%02x", bt);
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            
+        }
+    }
+}
+
